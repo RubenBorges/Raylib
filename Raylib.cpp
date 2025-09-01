37004156459d16bb86be6183e18dbf2665ce3e99
RubenBorges <ruben_borges711@hotmail.com>
2025-09-01 11:21:18 -0400

    Updated Comments

--- Raylib.cpp
+++ Raylib.cpp
@@ -1,5 +1,10 @@
 // Raylib.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+// Written by: Ruben Michael Borges
+// First Created : 1 September 2025
+// 
+// Description: A simple program to demonstrate basic collision detection, sound effects, and camera movement using a small and experimental game developed in C++ with the Raylib library.
+//
 
 #include "raylib.h"
 import std;
@@ -144,13 +149,3 @@ int main()
     return 0;
 } 
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
